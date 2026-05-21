3c4fca3cfecc15f313d31715b918f39612baf1f9
SergeyParkhachevIP-415 <asergerser126@gmail.com>
2026-05-21 09:43:25 +0700

    прописан главный файлик с роутами (аналогия с индекс в чистом html)

---
+++ src/router/index.tsx
@@ -0,0 +1,48 @@
+// router/index.tsx
+import { createBrowserRouter, Navigate } from 'react-router-dom';
+import { AppLayout } from '../layouts/AppLayout';
+import { DashboardPage } from '../pages/DashboardPage';
+import { SpreadsheetPage } from '../pages/SpreadsheetPage';
+import { ProfilePage } from '../pages/ProfilePage';
+import { NotFoundPage } from '../pages/NotFoundPage';
+import { ProtectedRoute } from '../components/ProtectedRoute';
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Navigate to="/dashboard" replace />,
+    },
+    {
+        element: <AppLayout />,
+        children: [
+            {
+                path: '/dashboard',
+                element: (
+                    <ProtectedRoute>
+                        <DashboardPage />
+                    </ProtectedRoute>
+                ),
+            },
+            {
+                path: '/documents/:documentId',
+                element: (
+                    <ProtectedRoute>
+                        <SpreadsheetPage />
+                    </ProtectedRoute>
+                ),
+            },
+            {
+                path: '/profile',
+                element: (
+                    <ProtectedRoute>
+                        <ProfilePage />
+                    </ProtectedRoute>
+                ),
+            },
+            {
+                path: '*',
+                element: <NotFoundPage />,
+            },
+        ],
+    },
+]);
